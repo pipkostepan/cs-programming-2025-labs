0a3def6d92e2ff8cf685c201b9e98ed5559ebfd2
pipkostepan <pipkostepan@gmail.com>
2025-10-17 12:24:22 +1000

    Update lab1.py

---
+++ labs/lab1/lab1.py
@@ -0,0 +1,83 @@
+1
+
+int = 1
+float = 9.39
+str = 'sisi'
+bool = True
+
+2
+
+name = input('your name:')
+print(name)
+age = input('your age:')
+print(age)
+
+3
+
+a = 342
+b = 56.2
+c = '43'
+c = int(c)
+p = a+b+c
+print(p)
+
+4
+
+a = 3
+b = 8
+print((a + 4*b)*(a - 3*b) + a**2)
+
+5
+
+a = int(input('ширина:'))
+b = int(input('высота:'))
+print(a*b, (a+b)*2)
+
+6
+
+print('*   *   *')
+print(' * * * *')
+print('  *   *')
+
+7
+
+a = 9
+b = 3
+print(a + b, b - a, a*b, a/b, a % b, b // a, b**a)
+print(a == b, a != b, b > a, b > a, b >= a, a >= b)
+
+8
+
+name = input()
+age = input()
+print(f'меня зовут {name}, мне {age} лет')
+
+9
+
+a = 'съешь еще'
+b = 'этих мягких'
+c = 'французких булок,'
+d = 'да'
+e = 'выпей чаю.'
+print(a + ' ' + b + ' ' + c + ' '+ d + ' ' + '' + e)
+
+10
+
+a = ' нет! да!'
+print(4*a)
+
+11
+
+a = int(input())
+b = int(input())
+c = int(input())
+g = (a + c) // b
+print(f'результат вычисления: {g}')
+
+12
+
+i = input()
+print(i[:4])
+print(i[7:])
+print(i[3:7])
+print(i[::-1])
